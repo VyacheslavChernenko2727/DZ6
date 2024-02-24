0a51afe2d35c38714669cb7aba1696bfff79307f
Vyacheslav <slavik-chernenko@yandex.ru>
2024-02-24 16:30:31 +0300

    +

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -4,18 +4,15 @@ public class PhoneBook {
     public static void main(String[] args) {
         Map<String, List<String>> phoneBook = new HashMap<>();
 
-        // Добавляем записи в телефонную книгу
         addEntry(phoneBook, "Иванов", "89439549574");
         addEntry(phoneBook, "Петров", "8953209853");
         addEntry(phoneBook, "Сидоров", "8942342322");
         addEntry(phoneBook, "Иванов", "8956667733");
         addEntry(phoneBook, "Петров", "89998887737");
 
-        // Сортировка записей по убыванию числа телефонов
         List<Map.Entry<String, List<String>>> sortedEntries = new ArrayList<>(phoneBook.entrySet());
         sortedEntries.sort((entry1, entry2) -> Integer.compare(entry2.getValue().size(), entry1.getValue().size()));
 
-        // Вывод отсортированных записей
         for (Map.Entry<String, List<String>> entry : sortedEntries) {
             String name = entry.getKey();
             List<String> phones = entry.getValue();
@@ -23,7 +20,6 @@ public class PhoneBook {
         }
     }
 
-    // Метод для добавления записи в телефонную книгу
     public static void addEntry(Map<String, List<String>> phoneBook, String name, String phoneNumber) {
         List<String> phones = phoneBook.getOrDefault(name, new ArrayList<>());
         phones.add(phoneNumber);
